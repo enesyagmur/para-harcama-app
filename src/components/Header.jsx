5ec91fba0bc5fd65be8e61019cd287a632571a4b
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-06-15 15:53:37 +0300

    para ekrann üstüne sabitlendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
   return (
     <div className="header">
       <div className="headerTop">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/800px-Elon_Musk_Royal_Society_%28crop2%29.jpg" />
-        <p>Spend Elon Musk' Money</p>
-      </div>
-      <div className="headerBottom">
         <h2>{money}</h2>
         <p>$</p>
+      </div>
+      <div className="headerBottom">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/800px-Elon_Musk_Royal_Society_%28crop2%29.jpg" />
+        <p>Spend Elon Musk' Money</p>
       </div>
     </div>
   );
