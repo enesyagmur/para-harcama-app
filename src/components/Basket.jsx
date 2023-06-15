9cb4c36ecaa525b3a2962a5895cc9d28b66a30c8
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-06-15 15:27:24 +0300

    sepette toplam tutar gösterimi

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,12 +1,30 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/heading-has-content */
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../context/Context";
 import Dropdown from "react-bootstrap/Dropdown";
 import "../style/basket.css";
 
 const Basket = () => {
   const { basket, setBasket } = useContext(GlobalContext);
+
+  useEffect(() => {
+    totalCalculate(basket);
+  }, [basket]);
+
+  const totalCalculate = (arr) => {
+    let total = 0;
+    arr.forEach((element) => {
+      if (element.piece === 1) {
+        total += element.price;
+      } else if (element.piece > 1) {
+        total += element.piece * element.price;
+      }
+    });
+    return total;
+  };
+
   return (
     <div className="basket">
       <Dropdown>
@@ -32,6 +50,7 @@ const Basket = () => {
                   </Dropdown.Item>
                 ))
               : null}
+            <div className="totalBasket">{totalCalculate(basket)}$</div>
           </div>
         </Dropdown.Menu>
       </Dropdown>
